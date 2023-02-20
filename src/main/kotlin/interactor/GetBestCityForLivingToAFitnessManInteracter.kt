b144d99af6b4ae317570e18465756a3ae5f862c8
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-20 23:46:58 +0200

    improve code

--- src/main/kotlin/interactor/GetBestCityForLivingToAFitnessManInteracter.kt
+++ src/main/kotlin/interactor/GetBestCityForLivingToAFitnessManInteracter.kt
@@ -3,10 +3,10 @@ package interactor
 import model.CityEntity
 
 class GetBestCityForLivingToAFitnessManInteracter(private val dataSource: CostOfLivingDataSource) {
-     operator fun invoke(countryName: String): CityEntity {
+    operator fun invoke(countryName: String): CityEntity {
         return dataSource.getAllCitiesData()
             .filter {
-                it.country.equals(countryName.trim(),true)  && excludeNullFoodsAndSalariesAndLowDataQuality(it)
+                filterCountries(it, countryName) && excludeNullFoodsAndSalariesAndLowDataQuality(it)
             }
             .sortedBy(::calculateTheRatioBetweenFoodPriceAndSalary)
             .takeIf { it.isNotEmpty() }
@@ -15,26 +15,39 @@ class GetBestCityForLivingToAFitnessManInteracter(private val dataSource: CostOf
 
     }
 
-    private fun excludeNullFoodsAndSalariesAndLowDataQuality(city: CityEntity) =
-        checkIfNotNullFoodPrices(city) && checkIfNotNullVegetablePrices(city) && checkIfNotNullServicePrice(city)&&
-                city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+    private fun excludeNullFoodsAndSalariesAndLowDataQuality(city: CityEntity): Boolean {
+        return checkIfNotNullFoodPrices(city) &&
+                checkIfNotNullVegetablePrices(city) &&
+                checkIfNotNullServicePrice(city) &&
+                city.averageMonthlyNetSalaryAfterTax != null &&
+                city.dataQuality
+    }
 
-    private fun checkIfNotNullVegetablePrices(city: CityEntity) =
-        with(city.fruitAndVegetablesPrices) {
+    private fun checkIfNotNullVegetablePrices(city: CityEntity): Boolean {
+        return with(city.fruitAndVegetablesPrices) {
             tomato1kg != null && potato1kg != null && onion1kg != null && lettuceOneHead != null
         }
+    }
 
-    private fun checkIfNotNullFoodPrices(city: CityEntity) =
-        with(city.foodPrices) {
+    private fun checkIfNotNullFoodPrices(city: CityEntity): Boolean {
+        return with(city.foodPrices) {
             riceWhite1kg != null && eggsRegular12 != null && chickenFillets1kg != null
         }
+    }
 
-    private fun checkIfNotNullServicePrice(city: CityEntity) =
-        with(city.servicesPrices){
+    private fun checkIfNotNullServicePrice(city: CityEntity): Boolean {
+        return with(city.servicesPrices) {
             fitnessClubMonthlyFeeForOneAdult != null
         }
+    }
 
-    private fun calculateTheRatioBetweenFoodPriceAndSalary(city: CityEntity) =
-        (city.fruitAndVegetablesPrices.getAverageVegetablesPrice() + city.foodPrices.getAverageFoodPrices() + city.servicesPrices.fitnessClubMonthlyFeeForOneAdult!!) /
+    private fun calculateTheRatioBetweenFoodPriceAndSalary(city: CityEntity): Double {
+        return (city.fruitAndVegetablesPrices.getAverageVegetablesPrice() + city.foodPrices.getAverageFoodPrices() + city.servicesPrices.fitnessClubMonthlyFeeForOneAdult!!) /
                 city.averageMonthlyNetSalaryAfterTax!!
+    }
+
+    private fun filterCountries(city: CityEntity, countryName: String): Boolean {
+        return city.country.equals(countryName.trim(), true)
+    }
+
 }
